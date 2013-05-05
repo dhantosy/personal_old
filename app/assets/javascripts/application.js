d88dd96dab062051bfcc2d56e34bb207a7757c37
dhantosy <dhantosy@gmail.com>
2013-05-06 00:37:55 +0800

    Changed Navbar BG

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,22 @@
 //= require jquery_ujs
 //= require_tree .
 
+$(document).ready(function(){
+	$('#navi').localScroll(800);
+	
+	//.parallax(xPosition, speedFactor, outerHeight) options:
+	//xPosition - Horizontal position of the element
+	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+	$('#intro').parallax("50%", 0.3);
+	$('#aboutme').parallax("50%", 0.1);
+	$('.logo').parallax("50%", 0.0);
+	$('#aboutme2').parallax("50%", 0.2);
+
+
+
+
+	
+
+
+})
